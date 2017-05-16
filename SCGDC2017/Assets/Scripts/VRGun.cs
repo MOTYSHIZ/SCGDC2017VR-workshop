51de66429b1e49e8a445b9bf33c948204a288e8c
Unknown <MOTYSHIZ@gmail.com>
2017-05-15 20:37:01 -0700

    Made the gun shoot

---
+++ SCGDC2017/Assets/Scripts/VRGun.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRTK.Examples
+{
+    public class VRGun : VRTK_InteractableObject
+    {
+        private GameObject bullet;
+        private float bulletSpeed = 1000f;
+        private float bulletLife = 5f;
+
+        public override void StartUsing(GameObject currentUsingObject)
+        {
+            base.StartUsing(currentUsingObject);
+            FireBullet();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            bullet = transform.Find("bullet").gameObject;
+            bullet.SetActive(false);
+        }
+
+        // Update is called once per frame
+        void FireBullet()
+        {
+            GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation);
+            bulletClone.SetActive(true);
+            Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
+            rb.AddForce(bulletClone.transform.forward * bulletSpeed);
+            Destroy(bulletClone, bulletLife);
+        }
+    }
+}
