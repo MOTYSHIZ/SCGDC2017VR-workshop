f85e33a9528170cd30d83fb8d4d9f960723983f7
Colin Trotter <skaterguyperson@yahoo.com>
2017-05-19 15:14:42 -0700

    Final version of FinsihedExample scene for workshop

--- SCGDC2017/Assets/Scripts/VRGun.cs
+++ SCGDC2017/Assets/Scripts/VRGun.cs
@@ -1,36 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
-namespace VRTK.Examples
+
+public class VRGun : VRTK_InteractableObject
 {
-    public class VRGun : VRTK_InteractableObject
+    private GameObject bullet;
+    private float bulletSpeed = 1000f;
+    private float bulletLife = 5f;
+
+    public override void StartUsing(GameObject currentUsingObject)
     {
-        private GameObject bullet;
-        private float bulletSpeed = 1000f;
-        private float bulletLife = 5f;
+        base.StartUsing(currentUsingObject);
+        FireBullet();
+    }
 
-        public override void StartUsing(GameObject currentUsingObject)
-        {
-            base.StartUsing(currentUsingObject);
-            FireBullet();
-        }
+    // Use this for initialization
+    void Awake()
+    {
+        bullet = transform.Find("bullet").gameObject;
+        bullet.SetActive(false);
+    }
 
-        // Use this for initialization
-        void Awake()
-        {
-            bullet = transform.Find("bullet").gameObject;
-            bullet.SetActive(false);
-        }
-
-        // Update is called once per frame
-        void FireBullet()
-        {
-            GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation);
-            bulletClone.SetActive(true);
-            Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
-            rb.AddForce(bulletClone.transform.forward * bulletSpeed);
-            Destroy(bulletClone, bulletLife);
-        }
+    // Update is called once per frame
+    void FireBullet()
+    {
+        GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation);
+        bulletClone.SetActive(true);
+        Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
+        rb.AddForce(bulletClone.transform.forward * bulletSpeed);
+        Destroy(bulletClone, bulletLife);
     }
 }
